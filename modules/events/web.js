992bf89a755d7d99964cacfe1fb9defe6fb584b3
zvshka <pushpurs@yandex.ru>
2021-04-24 12:06:11 +0300

    web

---
+++ modules/events/web.js
@@ -0,0 +1,16 @@
+const {Event} = require('../../handler');
+const express = require('express')
+const app = express()
+
+module.exports = class extends Event {
+    constructor() {
+        super('ready');
+    }
+
+    async run(client) {
+        app.get("/", (req, res) => {
+            res.send("OK")
+        })
+        app.listen(80)
+    }
+};
