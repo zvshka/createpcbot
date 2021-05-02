a9f0f1f956227d61e4c7fb79b3f5fd37b544e926
zvshka <pushpurs@yandex.ru>
2021-05-02 13:50:08 +0300

    fix

--- modules/events/start.js
+++ modules/events/start.js
@@ -1,11 +1,39 @@
 const {Event} = require('../../handler');
+const {MessageEmbed} = require('discord.js')
 
 module.exports = class extends Event {
     constructor() {
         super('ready');
     }
 
-    run(client) {
+    async run(client) {
         console.log(`Logged in as ${client.user.tag}`);
+
+        const start = new MessageEmbed()
+            .setTitle("Включаюсь")
+            .setTimestamp()
+            .setDescription("Происходит включение")
+
+        const guild = client.guilds.cache.get("725786415438364692")
+        const channel = guild.channels.cache.get("835439074571190292")
+        channel.send(start)
+
+        process.stdin.resume();//so the program will not close instantly
+
+        function exitHandler(options, exitCode) {
+            const embed = new MessageEmbed()
+                .setTitle("Выключение")
+                .setDescription(`Происходит выключение по коду ${exitCode}`)
+                .setTimestamp()
+            channel.send(embed).then(() => {
+                if (options.exit) process.exit();
+            })
+        }
+
+        process.on('exit', exitHandler.bind(null, {cleanup: true}));
+        process.on('SIGINT', exitHandler.bind(null, {exit: true}));
+        process.on('SIGUSR1', exitHandler.bind(null, {exit: true}));
+        process.on('SIGUSR2', exitHandler.bind(null, {exit: true}));
+        process.on('uncaughtException', exitHandler.bind(null, {exit: true}));
     }
 };
