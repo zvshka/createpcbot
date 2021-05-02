2eac681f2ad11a2afa101916a26017042706586c
zvshka <pushpurs@yandex.ru>
2021-05-02 19:04:17 +0300

    Giveaway prepare

--- modules/giveaway/commands/start.js
+++ modules/giveaway/commands/start.js
@@ -1,4 +1,8 @@
+const {MessageEmbed} = require("discord.js");
 const {Command} = require("../../../handler");
+const Giveaway = require("../../../schemas/Giveaway")
+const ms = require("ms");
+
 module.exports = class extends Command {
     constructor() {
         super("giveaway_start", {
@@ -10,6 +14,39 @@ module.exports = class extends Command {
         });
     }
     async run(message, args) {
+        if (!args[0] || !ms(args[0]) || ms(args[0]) < 5 * 60 * 1000) {
+            const error = new MessageEmbed()
+                .setTitle("Ошибка")
+                .setDescription("Вы указали время не корректно или оно меньше 5 минут")
+            return message.channel.send(error)
+        }
 
+        if (args[1] && isNaN(args[1])) {
+            const error = new MessageEmbed()
+                .setTitle("Ошибка")
+                .setDescription("Вы указали не корректное количество победителей")
+            return message.channel.send(error)
+        }
+
+        const time = ms(args[0])
+        const winners = parseInt(args[1]) || 1
+        const prize = args.length > 2 ? args.slice(2).join(" ") : "Уточните у админа, проводящего розыгрыш"
+
+        const embed = new MessageEmbed()
+            .setTitle("Розыгрыш призов!")
+            .setDescription(`${prize}\nПобедителей: ${winners}`)
+            .setTimestamp(Date.now() + time)
+        const gMessage = await message.channel.send(embed)
+
+        const giveaway = new Giveaway({
+            guild: message.guild.id,
+            channel: message.channel.id,
+            message: gMessage.id,
+            winners,
+            prize,
+            ends: Date.now() + time,
+            start: Date.now()
+        })
+        await giveaway.save().catch(_ => {})
     }
 }
--- schemas/Giveaway.js
+++ schemas/Giveaway.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema({
+    guild: String,
     channel: String,
     message: String,
+    winners: Number,
+    prize: String,
     ends: Date,
     start: Date
 }, {minimize: false})
