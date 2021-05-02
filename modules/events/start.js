404b78365e07c9a157ad86d9c9b6099f05085279
zvshka <pushpurs@yandex.ru>
2021-05-02 17:23:59 +0300

    fix

--- modules/events/start.js
+++ modules/events/start.js
@@ -36,8 +36,6 @@ module.exports = class extends Event {
         process.on('exit', exitHandler.bind(null, {cleanup: true}));
         process.on('SIGINT', exitHandler.bind(null, {exit: true}));
         process.on('SIGTERM', exitHandler.bind(null, {exit: true}));
-        process.on('SIGUSR1', exitHandler.bind(null, {exit: true}));
-        process.on('SIGUSR2', exitHandler.bind(null, {exit: true}));
         process.on('uncaughtException', exitHandler.bind(null, {exit: true}));
     }
 };
