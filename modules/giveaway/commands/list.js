965eee4bfc9bdcef8e45726a6272a32ee0bb9c2a
zvshka <pushpurs@yandex.ru>
2021-05-03 10:12:23 +0300

    Giveaway list command

--- modules/giveaway/commands/list.js
+++ modules/giveaway/commands/list.js
@@ -1,4 +1,8 @@
 const {Command} = require("../../../handler");
+const {MessageEmbed} = require('discord.js')
+const Giveaway = require('../../../schemas/Giveaway')
+const rm = require('discord.js-reaction-menu')
+const Utils = require('../../../Utils')
 module.exports = class extends Command {
     constructor() {
         super("giveaway_list", {
@@ -10,6 +14,29 @@ module.exports = class extends Command {
         });
     }
     async run(message, args) {
-
+        const giveaways = await Giveaway.find({guild: message.guild.id})
+        const embed = new MessageEmbed()
+            .setTitle("Список розыгрышей на сервере")
+        if (giveaways.length < 1) {
+            embed.setDescription("Сейчас ничего не проводится")
+            return message.channel.send(embed)
+        } else if (giveaways.length > 15) {
+            const divided = Utils.split(giveaways, 15)
+            const pages = divided.map(arr => {
+                const e = new MessageEmbed()
+                    .setTitle("Список розыгрышей на сервере")
+                for (let giveaway of arr) {
+                    e.addField(giveaway.prize, `**Начался**: ${giveaway.start}\n**Закончится**: ${giveaway.ends}\n**Победителей**: ${giveaway.winners}\n**Канал**:<#${giveaway.channel}>`)
+                }
+            })
+            return new rm.menu({
+                pages
+            })
+        } else {
+            for (let giveaway of giveaways) {
+                embed.addField(giveaway.prize, `**Начался**: ${giveaway.start}\n**Закончится**: ${giveaway.ends}\n**Победителей**: ${giveaway.winners}\n**Канал**:<#${giveaway.channel}>`)
+            }
+            return message.channel.send(embed)
+        }
     }
 }
