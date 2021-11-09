b44cfe4f57b33503c686534a58cf30a80a310a5c
zvshka <pushpurs@yandex.ru>
2021-11-09 18:27:47 +0300

    да

--- src/modules/general/commands/info.js
+++ src/modules/general/commands/info.js
@@ -17,7 +17,7 @@ export default class InfoCommand extends Command {
         const embed = new MessageEmbed()
             .setTitle("Информация о боте")
             .addField("Префикс", this.handler.prefix, true)
-            .addField("Версия", "2.0.0", true)
+            .addField("Версия", "Пошел нахуй пидорас", true)
             .addField("Время работы", this.format(process.uptime()), true)
         await message.channel.send(embed)
     }
