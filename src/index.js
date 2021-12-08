7d1b8e8b24fd22455f5f61c510dabdea6c0fb122
zvshka <pushpurs@yandex.ru>
2021-12-08 18:49:57 +0300

    да

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import path from "path"
 import mongoose from "mongoose"
-import {Client} from "discord.js";
+import {Client, Intents} from "discord.js";
 import {Handler} from "./handler"
 
-const client = new Client({intents: ['GUILDS', 'GUILD_MESSAGES']});
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+    ]
+});
 const handler = new Handler(client);
 
 handler.load(path.join(__dirname, './modules'), {
