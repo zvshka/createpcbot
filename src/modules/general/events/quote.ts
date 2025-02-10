456a2d949d98c34d22cbb0ce72059f8aa3a2e4c2
zvshka <pushpurs@yandex.ru>
2025-02-10 21:46:11 +0300

    Новые шрифты, хз

--- src/modules/general/events/quote.ts
+++ src/modules/general/events/quote.ts
@@ -18,7 +18,7 @@ import axios from 'axios';
 // GlobalFonts.registerFromPath('./fonts/GoogleSans-Regular.ttf', 'Google Sans Regular');
 // GlobalFonts.registerFromPath('./fonts/GoogleSans-Italic.ttf', 'Google Sans Italic');
 GlobalFonts.loadFontsFromDir('./fonts');
-console.log(GlobalFonts.families.map(family => `${family.family} ${family.styles.join(" ")}`));
+console.log(GlobalFonts.families.map(family => `${family.family} ${family.styles.map(s => s.style)}`));
 
 const mainTextSize = 52
 const secondaryTextSize = 42
